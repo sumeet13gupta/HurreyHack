874e378ae5869042b6d45b85a23c7ad70c1b066f
sgsumit <sumeet.gupta@orchids.edu.in>
2022-11-09 20:22:54 +0530

    An Idea file is added as README.md

--- Text_recognition.py
+++ Text_recognition.py
@@ -18,6 +18,10 @@ load_dotenv()
 COMPUTER_VISION_ENDPOINT = os.getenv("COMPUTER_VISION_ENDPOINT")
 COMPUTER_VISION_SUBSCRIPTION_KEY = os.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY")
 #print(COMPUTER_VISION_ENDPOINT)
+
+
+
+
 missing_env = False
 # Add your Computer Vision subscription key and endpoint to your environment variables.
 if 'COMPUTER_VISION_ENDPOINT' in os.environ:
@@ -38,22 +42,20 @@ if missing_env:
     print("**Restart your shell or IDE for changes to take effect.**")
     sys.exit()
 
+
+#Text recognition Api goes here .... :)
 text_recognition_url = endpoint + "/vision/v3.1/read/analyze"
 
 # Set image_url to the URL of an image that you want to recognize.
-image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
+image_url = "https://github.com/sumeet13gupta/HurreyHack/blob/main/pic_phi.jpg?raw=true"
+# "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
 # 
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 data = {'url': image_url}
-response = requests.post(
-    text_recognition_url, headers=headers, json=data)
+response = requests.post(text_recognition_url, headers=headers, json=data)
 response.raise_for_status()
 
-# Extracting text requires two API calls: One call to submit the
-# image for processing, the other to retrieve the text found in the image.
-
-# Holds the URI used to retrieve the recognized text.
 operation_url = response.headers["Operation-Location"]
 
 # The recognized text isn't immediately available, so poll to wait for completion.
@@ -64,7 +66,7 @@ while (poll):
         response.headers["Operation-Location"], headers=headers)
     analysis = response_final.json()
     
-    print(json.dumps(analysis, indent=4))
+    print(json.dumps(analysis, indent=2))
 
     time.sleep(1)
     if ("analyzeResult" in analysis):
@@ -81,11 +83,19 @@ if ("analyzeResult" in analysis):
 # Display the image and overlay it with the extracted text.
 image = Image.open(BytesIO(requests.get(image_url).content))
 ax = plt.imshow(image)
+all_obj = []
+numbers = []
+
 for polygon in polygons:
     vertices = [(polygon[0][i], polygon[0][i+1])
                 for i in range(0, len(polygon[0]), 2)]
     text = polygon[1]
+    all_obj.append(text)
+    if(text.isnumeric()):
+        numbers.append(float(text))
     patch = Polygon(vertices, closed=True, fill=False, linewidth=2, color='y')
     ax.axes.add_patch(patch)
-    plt.text(vertices[0][0], vertices[0][1], text, fontsize=20, va="top")
+    plt.text(vertices[0][0], vertices[0][1], text, fontsize=8, va="top")
+print(numbers)
+print(all_obj)
 plt.show()
